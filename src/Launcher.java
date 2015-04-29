40877e54e4e14fe5926cf6fe23c1318bc23406f4
Kewin Dousse <kewin.d@websud.ch>
2015-04-29 10:13:12 +0200

    Most unefficient algorithm I've ever made

--- src/Launcher.java
+++ src/Launcher.java
@@ -27,11 +27,12 @@ public class Launcher {
 		int i;
 		File file = new File("test.mp4");
 		BufferedImage frame;
+
 		String path="frames/";
 		String name="frame_";
 		String ext="bmp";
-        for (i = 0; true; i++) { 
-        	System.out.println("Computing frame " + i);
+		for (i = 0; i < 2; i++) { 
+			System.out.println("Computing frame " + i);
 			try {
 				frame = FrameGrab.getFrame(file, i);
 				ImageIO.write(frame, ext, new File(path + name + i + "." + ext));
@@ -41,48 +42,79 @@ public class Launcher {
 			} catch (ArrayIndexOutOfBoundsException e) {
 				break;
 			}
-        }
-        System.out.println("Finished extracting " + i + " frames in " + (System.currentTimeMillis() - time)+" milliseconds");
-        
-        PixelImage image;
-        try {
-			frame = FrameGrab.getFrame(file, 0);
-			BufferedRGB24Image out = new BufferedRGB24Image(new BufferedImage(frame.getWidth(), frame.getHeight(), frame.getType()));
-	        for (int j = 0; j < i; j++) {
-	        	try {
-					image = ImageLoader.load(path + name + j + "." + ext);
-					out.putSample(2, 1, 1, 123); // Blue
-					// TODO : Process
-				} catch (InvalidFileStructureException | InvalidImageIndexException
-						| UnsupportedTypeException | IOException e) {
+		}
+		System.out.println("Finished extracting " + i + " frames in " + (System.currentTimeMillis() - time)+" milliseconds");
+
+		PixelImage image;
+		BufferedImage frame2;
+		try {
+			frame2 = FrameGrab.getFrame(file, 0);
+			int w = frame2.getWidth();
+			int h = frame2.getHeight();
+
+			int[][][][] pixels = new int[i-1][][][];
+			try {
+				frame = FrameGrab.getFrame(file, 0);
+				BufferedRGB24Image out = new BufferedRGB24Image(new BufferedImage(frame.getWidth(), frame.getHeight(), frame.getType()));
+				for (int nbFrame = 0; nbFrame < i-1; nbFrame++) {
+					pixels[nbFrame] = new int[w][][];
+					for (int x = 0; x < w; x++) {
+						pixels[nbFrame][x] = new int[h][];
+						for (int y = 0; y < h; y++) {
+							pixels[nbFrame][x][y] = new int[3];
+							for (int channel = 0; channel < 3; channel++) {
+								image = ImageLoader.load(path + name + nbFrame + "." + ext);
+								out.putSample(nbFrame, x, y, 123); // Blue
+							}
+							System.out.println("y: " + y);
+						}
+						System.out.println("x: " + x);
+					}
+					System.out.println("frame " + nbFrame);
+				}
+				PNGCodec codec = new PNGCodec();
+				try {
+					codec.setFile("out.png", CodecMode.SAVE);
+					codec.setImage(out);
+					codec.setCompressionLevel(Deflater.BEST_COMPRESSION);
+					try {
+						codec.process();
+						System.out.println("Finished");
+					} catch (OperationFailedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				} catch (UnsupportedCodecModeException | IOException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-	        }
-	        PNGCodec codec = new PNGCodec();
-	        try {
-				codec.setFile("out.png", CodecMode.SAVE);
-		        codec.setImage(out);
-		        codec.setCompressionLevel(Deflater.BEST_COMPRESSION);
-		        try {
-					codec.process();
-					System.out.println("Finished");
-				} catch (OperationFailedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			} catch (UnsupportedCodecModeException | IOException e) {
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			} catch (JCodecException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			} catch (InvalidFileStructureException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (InvalidImageIndexException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (UnsupportedTypeException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-		} catch (IOException e1) {
+		} catch (IOException e2) {
 			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		} catch (JCodecException e1) {
+			e2.printStackTrace();
+		} catch (JCodecException e2) {
 			// TODO Auto-generated catch block
-			e1.printStackTrace();
+			e2.printStackTrace();
 		}
-    
+
+
+
+
 	}
 
 }
