e1237862f27aae6a04190a01ed5ccb5cba7f9d2d
Kewin Dousse <kewin.d@websud.ch>
2015-04-29 13:37:21 +0200

    Code style and unused imports

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,19 +1,11 @@
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.zip.Deflater;
 
-import javax.imageio.ImageIO;
-
 import net.sourceforge.jiu.codecs.CodecMode;
-import net.sourceforge.jiu.codecs.ImageLoader;
-import net.sourceforge.jiu.codecs.InvalidFileStructureException;
-import net.sourceforge.jiu.codecs.InvalidImageIndexException;
 import net.sourceforge.jiu.codecs.PNGCodec;
 import net.sourceforge.jiu.codecs.UnsupportedCodecModeException;
-import net.sourceforge.jiu.codecs.UnsupportedTypeException;
-import net.sourceforge.jiu.data.PixelImage;
 import net.sourceforge.jiu.gui.awt.BufferedRGB24Image;
 import net.sourceforge.jiu.ops.OperationFailedException;
 
@@ -21,16 +13,14 @@ import org.jcodec.api.FrameGrab;
 import org.jcodec.api.JCodecException;
 
 public class Launcher {
-	static String path = "frames/";
-	static String name = "frame_";
-	static String ext = "bmp";
+	static final int RED_CHANNEL = 0;
+	static final int GREEN_CHANNEL = 1;
+	static final int BLUE_CHANNEL = 2;
 
 	public static void main(String[] args) {
-		long time = System.currentTimeMillis();
-		int frameNb;
+		long startingTime = System.currentTimeMillis();
 		File videoFile = new File("test.mp4");
 		BufferedImage frame;
-		BufferedRGB24Image image;
 		try {
 			frame = FrameGrab.getFrame(videoFile, 0);
 			
@@ -42,23 +32,25 @@ public class Launcher {
 				pixels[x] = new int[h][];
 				for (int y = 0; y < h; y++) {
 					pixels[x][y] = new int[3];
-					pixels[x][y][0] = 0;
-					pixels[x][y][1] = 0;
-					pixels[x][y][2] = 0;
+					pixels[x][y][RED_CHANNEL] = 0;
+					pixels[x][y][GREEN_CHANNEL] = 0;
+					pixels[x][y][BLUE_CHANNEL] = 0;
 				}
 			}
 
+			int frameNb;
 			// For each frame
-			for (frameNb = 0; frameNb < 2; frameNb++) { 
-				System.out.println("Computing frame " + frameNb);
+			for (frameNb = 0; true; frameNb++) { 
+				System.out.println("Sampling frame " + frameNb);
+				BufferedRGB24Image image;
 				try {
 					image = new BufferedRGB24Image(FrameGrab.getFrame(videoFile, frameNb));
 					// Adding sample
 					for (int x = 0; x < w; x++) {
 						for (int y = 0; y < h; y++) {
-							pixels[x][y][0] += image.getSample(0, x, y);
-							pixels[x][y][1] += image.getSample(1, x, y);
-							pixels[x][y][2] += image.getSample(2, x, y);
+							pixels[x][y][RED_CHANNEL] += image.getSample(RED_CHANNEL, x, y);
+							pixels[x][y][GREEN_CHANNEL] += image.getSample(GREEN_CHANNEL, x, y);
+							pixels[x][y][BLUE_CHANNEL] += image.getSample(BLUE_CHANNEL, x, y);
 						}
 					}
 				} catch (IOException | JCodecException e) {
@@ -71,23 +63,27 @@ public class Launcher {
 			
 			BufferedRGB24Image out = new BufferedRGB24Image(new BufferedImage(frame.getWidth(), frame.getHeight(), frame.getType()));
 			
+			System.out.println("Computing average");
 			// Saving average
 			for (int x = 0; x < w; x++) {
 				for (int y = 0; y < h; y++) {
-					out.putSample(0, x, y, pixels[x][y][0] / frameNb); // Red
-					out.putSample(1, x, y, pixels[x][y][1] / frameNb); // Green
-					out.putSample(2, x, y, pixels[x][y][2] / frameNb); // Blue
+					out.putSample(RED_CHANNEL, x, y, pixels[x][y][RED_CHANNEL] / frameNb); // Red
+					out.putSample(GREEN_CHANNEL, x, y, pixels[x][y][GREEN_CHANNEL] / frameNb); // Green
+					out.putSample(BLUE_CHANNEL, x, y, pixels[x][y][BLUE_CHANNEL] / frameNb); // Blue
 				}
 			}
 			
 			PNGCodec codec = new PNGCodec();
 			try {
+				System.out.println("Saving result");
 				codec.setFile("out.png", CodecMode.SAVE);
 				codec.setImage(out);
 				codec.setCompressionLevel(Deflater.BEST_COMPRESSION);
 				try {
 					codec.process();
+					long endingTime = System.currentTimeMillis();
 					System.out.println("Finished");
+					System.out.println("Total time : " + (endingTime - startingTime)/1000 + " seconds");
 				} catch (OperationFailedException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
